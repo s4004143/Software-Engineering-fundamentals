a94f13ef962ca55cb84f996275b107fcf9363151
Kevin Yu <s4004143@student.rmit.edu.au>
2024-10-21 05:58:48 +1100

    added skeleton code

--- test.java
+++ test.java
@@ -1,6 +1,38 @@
-public class test {
+import java.util.arrayList;
+import java.util.Date;
 
-    public int test;
+public class Prescription {
 
-    // test
+    private int prescID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private float sphere;
+    private float axis;
+    private float cylinder;
+    private Date examinationDate;
+    private String optometrist;
+    private String[] remarkTypes= {"Client", "Optometrist"};
+    private ArrayList <String> postRemarks= new ArrayList<>();
+
+    public boolean addPrescription()
+    {
+
+        return true;
+    }
+    
+
+    public boolean addRemark()
+    {
+
+
+        return true;
+    }
+
+public static void  main(String[] args) {
+
+
+
+}
+
 }
